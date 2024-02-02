219988671ebd21bbbf23a7ef9328420c108b0a6f
harshal98 <harshal.somani1998@gmail.com>
2024-02-02 16:12:02 +0530

    acc Multiple timeframe

--- src/Components/Aco.tsx
+++ src/Components/Aco.tsx
@@ -1,4 +1,4 @@
-import { BollingerBands } from "@debut/indicators";
+import { AC } from "@debut/indicators";
 import axios, { AxiosResponse } from "axios";
 import {
   Table,
@@ -17,15 +17,14 @@ import Timer from "./Timer";
 let FuturePairs: string[] = [];
 
 function Aco() {
-  //let [data, setdata] = useState<Klinedata>([]);
   let [resetcd, setresetcd] = useState(0);
   let [Acodata, setAcodata] = useState<
     {
       pair: string;
       percentrank: number;
-      bb15m: string;
-      bb1h: string;
-      bb4h: string;
+      acc15m: string;
+      acc1h: string;
+      acc4h: string;
     }[]
   >([]);
 
@@ -33,21 +32,9 @@ function Aco() {
     getAllTimeKline().then((res) => {
       let temp1: {
         futurepair: string;
-        BB15mArray: {
-          lower: number;
-          middle: number;
-          upper: number;
-        }[];
-        BB1hArray: {
-          lower: number;
-          middle: number;
-          upper: number;
-        }[];
-        BB4hArray: {
-          lower: number;
-          middle: number;
-          upper: number;
-        }[];
+        acc15m: boolean;
+        acc1h: boolean;
+        acc4h: boolean;
         lastprice: number;
       }[] = [];
       let lastprice = 0;
@@ -57,58 +44,54 @@ function Aco() {
             res.m15m.filter((i) => i.pair == futurepair)[0].kline.length - 1
           ].c
         );
-        let BB15mArray = getBB(
-          res.m15m.filter((i) => i.pair == futurepair)[0].kline
+        let acc15m = getAc(
+          {
+            pair: futurepair,
+            kline: res.m15m.filter((i) => i.pair == futurepair)[0].kline,
+          },
+          "15m"
         );
-        let BB1hArray = getBB(
-          res.h1.filter((i) => i.pair == futurepair)[0].kline
+        let acc1h = getAc(
+          {
+            pair: futurepair,
+            kline: res.h1.filter((i) => i.pair == futurepair)[0].kline,
+          },
+          "1h"
         );
-        let BB4hArray = getBB(
-          res.h4.filter((i) => i.pair == futurepair)[0].kline
+        let acc4h = getAc(
+          {
+            pair: futurepair,
+            kline: res.h4.filter((i) => i.pair == futurepair)[0].kline,
+          },
+          "4h"
         );
 
-        temp1.push({ futurepair, BB15mArray, BB1hArray, BB4hArray, lastprice });
+        temp1.push({ futurepair, acc15m, acc1h, acc4h, lastprice });
       });
+      console.log(temp1);
 
       let temp2 = temp1
         .map((i) => {
-          let bb15m =
-            i.BB15mArray[i.BB15mArray.length - 1].upper >
-              i.BB15mArray[i.BB15mArray.length - 2].upper ||
-            i.BB15mArray[i.BB15mArray.length - 1].upper > i.lastprice * 1.05
-              ? "Yes"
-              : "No";
-          let bb1h =
-            i.BB1hArray[i.BB1hArray.length - 1].upper >
-              i.BB1hArray[i.BB1hArray.length - 2].upper ||
-            i.BB1hArray[i.BB1hArray.length - 1].upper > i.lastprice * 1.1
-              ? "Yes"
-              : "No";
-          let bb4h =
-            i.BB4hArray[i.BB4hArray.length - 1].upper >
-              i.BB4hArray[i.BB4hArray.length - 2].upper ||
-            i.BB4hArray[i.BB4hArray.length - 1].upper > i.lastprice * 1.1
-              ? "Yes"
-              : "No";
-          return { pair: i.futurepair, bb15m, bb1h, bb4h };
+          let acc15m = i.acc15m ? "Yes" : "No";
+          let acc1h = i.acc1h ? "Yes" : "No";
+          let acc4h = i.acc4h ? "Yes" : "No";
+          return { pair: i.futurepair, acc15m, acc4h, acc1h };
         })
-        .filter((i) => i.bb15m == "Yes" && i.bb1h == "Yes" && i.bb4h == "Yes");
+        .filter(
+          (i) => i.acc15m == "Yes" || i.acc1h == "Yes" || i.acc4h == "Yes"
+        );
 
       get24hrpercent().then((res) => {
+        let temp3 = temp2.map((i) => {
+          return {
+            ...i,
+            percentrank: res.findIndex((r) => {
+              return r.pair == i.pair;
+            }),
+          };
+        });
         setAcodata(
-          temp2
-            .map((i) => {
-              return {
-                ...i,
-                percentrank: res.findIndex((r) => {
-                  return r.pair == i.pair;
-                }),
-              };
-            })
-            .sort((i, j) => {
-              if (i.percentrank > j.percentrank) return 1;
-              else return -1;
-            })
+          sortdata(sortdata(sortdata(temp3, "acc15m"), "acc1h"), "acc4h")
         );
       });
     });
@@ -166,9 +149,9 @@ function Aco() {
               <Th>Index</Th>
               <Th>Pair</Th>
               <Th>DailyRank</Th>
-              <Th>BB4h</Th>
-              <Th>BB1h</Th>
-              <Th>BB4h</Th>
+              <Th>15m-Acc</Th>
+              <Th>1h-Acc</Th>
+              <Th>4h-Acc</Th>
             </Tr>
           </Thead>
           <Tbody>
@@ -178,9 +161,9 @@ function Aco() {
 
                 <Td>{i.pair}</Td>
                 <Td>{i.percentrank}</Td>
-                <Td>{i.bb15m}</Td>
-                <Td>{i.bb1h}</Td>
-                <Td>{i.bb4h}</Td>
+                <Td>{i.acc15m}</Td>
+                <Td>{i.acc1h}</Td>
+                <Td>{i.acc4h}</Td>
               </Tr>
             ))}
           </Tbody>
@@ -206,14 +189,19 @@ async function getKlineData(time: string) {
     );
   });
   let res = await axios.all(promisarray);
-  let temp: { pair: string; kline: { c: number }[] }[] = [];
+  let temp: {
+    pair: string;
+    kline: { c: number; high: number; low: number }[];
+  }[] = [];
   res.forEach((r: AxiosResponse) => {
     let pairUrl = String(r.request.responseURL);
     pairUrl = pairUrl.substring(pairUrl.lastIndexOf("=") + 1);
-    let hc: { c: number }[] = [];
+    let hc: { c: number; high: number; low: number }[] = [];
 
     r.data.forEach((i: any[]) =>
       hc.push({
+        high: Number(i[1]),
+        low: Number(i[2]),
         c: Number(i[4]),
       })
     );
@@ -231,19 +219,133 @@ async function getAllTimeKline() {
   return { m15m, h1, h4 };
 }
 
-function getBB(klinedata: { c: number }[]) {
-  let BB = new BollingerBands();
-  let BBarray: {
-    lower: number;
-    middle: number;
-    upper: number;
-  }[] = [];
-  klinedata.forEach((kline) => {
-    let val = BB.nextValue(kline.c);
+function getACmax(aclist: number[]) {
+  let lowaclist: { indx: number; val: number }[] = [];
+  let highaclist: { indx: number; val: number }[] = [];
+
+  for (let x = aclist.length - 1; x >= 0; x--) {
+    if (aclist[x] < 0) {
+      lowaclist.push({
+        indx: x,
+        val: aclist[x],
+      });
+    }
+  }
+  for (let x = aclist.length - 1; x >= 0; x--) {
+    if (aclist[x] > 0) {
+      highaclist.push({
+        indx: x,
+
+        val: aclist[x],
+      });
+    }
+  }
+
+  let acListMaxLows: { max: number }[] = [];
+  if (lowaclist.length > 0) {
+    let max = lowaclist[0].val;
+
+    let lastindex = lowaclist[0].indx;
+    for (let x = 1; x < lowaclist.length; x++) {
+      if (lastindex == lowaclist[x].indx + 1) {
+        if (max > lowaclist[x].val) {
+          max = lowaclist[x].val;
+        }
+        lastindex--;
+      } else {
+        acListMaxLows.push({ max });
+        lastindex = lowaclist[x].indx;
+        max = lowaclist[x].val;
+      }
+      if (x == lowaclist.length - 1) acListMaxLows.push({ max });
+    }
+  }
+
+  let acListMaxHighs: { max: number }[] = [];
+  if (highaclist.length > 0) {
+    let max = highaclist[0].val;
+
+    let lastindex = highaclist[0].indx;
+    for (let x = 1; x < highaclist.length; x++) {
+      if (lastindex == highaclist[x].indx + 1) {
+        if (max < highaclist[x].val) {
+          max = highaclist[x].val;
+        }
+        lastindex--;
+      } else {
+        acListMaxHighs.push({ max });
+        lastindex = highaclist[x].indx;
+        max = highaclist[x].val;
+      }
+      if (x == highaclist.length - 1) acListMaxHighs.push({ max });
+    }
+  }
+  let lastmax = 0;
+
+  if (aclist[aclist.length - 1] > 0) {
+    lastmax = acListMaxHighs[0].max;
+    acListMaxHighs = acListMaxHighs.slice(1, acListMaxHighs.length);
+  } else {
+    lastmax = acListMaxLows[0].max;
+    acListMaxLows = acListMaxLows.slice(1, acListMaxLows.length);
+  }
+  return {
+    high: acListMaxHighs.slice(0, 2),
+    low: acListMaxLows.slice(0, 2),
+    lastmax,
+  };
+}
+
+function getAc(
+  item: { pair: string; kline: { high: number; low: number }[] },
+  _timeframe: string
+) {
+  let ac = new AC();
+  let aclist: number[] = [];
+  item.kline.forEach((i) => {
+    let val = ac.nextValue(i.high, i.low);
     if (val) {
-      BBarray.push(val);
+      aclist.push(val);
     }
   });
-  return BBarray;
+  let result = getACmax(aclist);
+  if (result.high.length > 0 && result.low.length > 0) {
+    let countH = 0;
+    let prev = result.high[0].max;
+    result.high.forEach((i) => {
+      if (i.max > prev) countH++;
+      prev = i.max;
+    });
+    let countL = 0;
+    prev = result.low[0].max;
+    result.low.forEach((i) => {
+      if (i.max < prev) countL++;
+      prev = i.max;
+    });
+    console.log(
+      item.pair,
+      _timeframe,
+      result,
+      result.lastmax > result.low[0].max
+    );
+
+    if (countL > 0 && result.lastmax > result.low[0].max) return true;
+  }
+  return false;
+}
+function sortdata(
+  t: {
+    pair: string;
+    percentrank: number;
+    acc15m: string;
+    acc1h: string;
+    acc4h: string;
+  }[],
+  sortby: string
+) {
+  return t.sort((i: any, j: any) => {
+    if (i[sortby] > j[sortby]) return -1;
+    else return 1;
+  });
 }
 export default Aco;
